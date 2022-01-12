ebe8d3468be5db9b3ea4ceab8d5b5dc8e880f465
typhonrt <support@typhonjs.io>
2022-01-12 03:51:23 -0800

    remove standard components

--- .rollup/local/externalPathsNPM.js
+++ .rollup/local/externalPathsNPM.js
@@ -1,7 +1,6 @@
 export const externalPathsNPM = {
    '@typhonjs-svelte/lib/action': '@typhonjs-fvtt/svelte/action',
    '@typhonjs-svelte/lib/component/core': '@typhonjs-fvtt/svelte/component/core',
-   '@typhonjs-svelte/lib/component/standard': '@typhonjs-fvtt/svelte/component/standard',
    '@typhonjs-svelte/lib/handler': '@typhonjs-fvtt/svelte/handler',
    '@typhonjs-svelte/lib/helper': '@typhonjs-fvtt/svelte/helper',
    '@typhonjs-svelte/lib/plugin/data': '@typhonjs-fvtt/svelte/plugin/data',
