6319f9f6cc74bc9cc11875cfbecd690a9038d673
typhonrt <support@typhonjs.io>
2022-06-29 20:16:34 -0700

    comments

--- src/store/storeCallback.js
+++ src/store/storeCallback.js
@@ -1,24 +1,30 @@
 import { isWritableStore } from '@typhonjs-svelte/lib/store';
 
 /**
- * Wraps a writable stores set method invoking a callback after the store is set. This allows parent / child
- * relationships between stores to update directly without having to subscribe to the child store. This is a particular
- * powerful pattern when the `setCallback` is a debounced function that syncs a parent store and / or serializes data.
+ * Wraps a writable stores set method invoking a callback after the store is set. This allows hard coupled parent /
+ * child relationships between stores to update directly without having to subscribe to the child store. This is a
+ * particular powerful pattern when the `setCallback` is a debounced function that syncs a parent store and / or
+ * serializes data.
  *
- * @param {import('svelte/store').Writable} store - A store to wrap.
+ * Note: Do consider carefully if this is an optimum solution; this is a quick implementation helper, but a better
+ * solution is properly managing store relationships through subscription.
  *
- * @param {(store?: import('svelte/store').Writable, value?: *) => void} setCallback - A callback to invoke after store
- *                                                                                     set.
+ * @template T
  *
- * @returns {import('svelte/store').Writable} Wrapped store.
+ * @param {import('svelte/store').Writable<T>} store - A store to wrap.
+ *
+ * @param {(store?: import('svelte/store').Writable<T>, value?: T) => void} setCallback - A callback to invoke after
+ *                                                                                        store set.
+ *
+ * @returns {import('svelte/store').Writable<T>} Wrapped store.
  */
 export function storeCallback(store, setCallback)
 {
    if (!isWritableStore(store)) { throw new TypeError(`'store' is not a writable store.`); }
    if (typeof setCallback !== 'function') { throw new TypeError(`'setCallback' is not a function.`); }
 
-   /** @type {import('svelte/store').Writable} */
-   const wrapper = {
+   /** @type {import('svelte/store').Writable<T>} */
+   return {
       set: (value) => {
          store.set(value);
          setCallback(store, value);
@@ -28,8 +34,4 @@ export function storeCallback(store, setCallback)
 
       update: typeof store.update === 'function' ? store.update : void 0
    };
-
-   Object.freeze(wrapper);
-
-   return wrapper;
 }
