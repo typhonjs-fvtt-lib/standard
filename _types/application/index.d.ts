df1627714b0beaf9975cfbc34615069a808b77df
typhonrt <support@typhonjs.io>
2022-09-26 23:11:22 -0700

    _types dist

--- _types/application/index.d.ts
+++ _types/application/index.d.ts
@@ -5,7 +5,7 @@ declare class TJSContextMenu {
     /**
      * Stores any active context menu.
      */
-    static "__#78685@#contextMenu": any;
+    static "__#122823@#contextMenu": any;
     /**
      * Creates and manages a game wide context menu.
      *
