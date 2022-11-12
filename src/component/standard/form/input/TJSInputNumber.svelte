19a3ee0461de9a8ff23e74c024a0733d3679d84d
typhonrt <support@typhonjs.io>
2022-11-12 00:09:36 -0800

    Added TJSInputNumber / storeIsValid

--- src/component/standard/form/input/TJSInputNumber.svelte
+++ src/component/standard/form/input/TJSInputNumber.svelte
@@ -13,42 +13,44 @@
     * --tjs-input-cursor
     * --tjs-input-height
     * --tjs-input-padding
-    * --tjs-input-placeholder-color
+    * --tjs-input-placeholder-color;
     * --tjs-input-text-align
     * --tjs-input-value-invalid-color
     * --tjs-input-width
     *
-    * --tjs-input-text-border
-    * --tjs-input-text-border-radius
-    * --tjs-input-text-background
-    * --tjs-input-text-cursor
-    * --tjs-input-text-height
-    * --tjs-input-text-padding
-    * --tjs-input-text-placeholder-color
-    * --tjs-input-text-text-align
-    * --tjs-input-text-value-invalid-color
-    * --tjs-input-text-width
+    * --tjs-input-number-border
+    * --tjs-input-number-border-radius
+    * --tjs-input-number-background
+    * --tjs-input-number-cursor
+    * --tjs-input-number-height
+    * --tjs-input-number-padding
+    * --tjs-input-number-placeholder-color
+    * --tjs-input-number-text-align
+    * --tjs-input-number-value-invalid-color
+    * --tjs-input-number-width
     */
 
-   import { writable }          from 'svelte/store';
+   import { writable }  from 'svelte/store';
 
    import {
       applyStyles,
-      autoBlur }                from '@typhonjs-svelte/lib/action';
+      autoBlur }        from '@typhonjs-svelte/lib/action';
 
-   import { localize }          from '@typhonjs-svelte/lib/helper';
+   import { localize }  from '@typhonjs-svelte/lib/helper';
 
    import {
       isReadableStore,
-      isWritableStore }         from '@typhonjs-svelte/lib/store';
+      isWritableStore } from '@typhonjs-svelte/lib/store';
 
-   import { isObject }          from '@typhonjs-svelte/lib/util';
+   import { isObject }  from '@typhonjs-svelte/lib/util';
 
    export let input = void 0;
-   export let type = void 0;
    export let disabled = void 0;
    export let options = void 0;
+   export let max = void 0;
+   export let min = void 0;
    export let placeholder = void 0;
+   export let step = void 0;
    export let store = void 0;
    export let storeIsValid = void 0;
    export let styles = void 0;
@@ -56,30 +58,10 @@
 
    const localOptions = {
       blurOnEnterKey: true,
-      cancelOnEscKey: false,
-      clearOnEscKey: false
+      cancelOnEscKey: false
    }
 
    let inputEl;
-
-   $: {
-      type = isObject(input) && typeof input.type === 'string' ? input.type :
-       typeof type === 'string' ? type : 'text';
-
-      switch (type)
-      {
-         case 'email':
-         case 'password':
-         case 'search':
-         case 'text':
-         case 'url':
-            break;
-
-         default:
-            throw new Error(
-             `'TJSInputText only supports text input types: 'email', 'password', 'search', 'text', 'url'.`);
-      }
-   }
 
    $: disabled = isObject(input) && typeof input.disabled === 'boolean' ? input.disabled :
     typeof disabled === 'boolean' ? disabled : false;
@@ -90,14 +72,25 @@
 
       if (typeof options?.blurOnEnterKey === 'boolean') { localOptions.blurOnEnterKey = options.blurOnEnterKey; }
       if (typeof options?.cancelOnEscKey === 'boolean') { localOptions.cancelOnEscKey = options.cancelOnEscKey; }
-      if (typeof options?.clearOnEscKey === 'boolean') { localOptions.clearOnEscKey = options.clearOnEscKey; }
    }
+
+   $: max = isObject(input) && typeof input.max === 'number' ? input.max :
+    typeof max === 'number' ? max : void 0;
+
+   $: min = isObject(input) && typeof input.min === 'number' ? input.min :
+    typeof min === 'number' ? min : void 0;
 
    $: placeholder = isObject(input) && typeof input.placeholder === 'string' ? localize(input.placeholder) :
     typeof placeholder === 'string' ? localize(placeholder) : void 0;
 
+   $: step = isObject(input) && typeof input.step === 'number' ? input.step :
+    typeof step === 'number' ? step : void 0;
+
    $: store = isObject(input) && isWritableStore(input.store) ? input.store :
     isWritableStore(store) ? store : writable(void 0);
+
+   $: storeIsValid = isObject(input) && isReadableStore(input.storeIsValid) ? input.storeIsValid :
+    isReadableStore(storeIsValid) ? storeIsValid : writable(true);
 
    $: storeIsValid = isObject(input) && isReadableStore(input.storeIsValid) ? input.storeIsValid :
     isReadableStore(storeIsValid) ? storeIsValid : writable(true);
@@ -108,11 +101,18 @@
    $: efx = isObject(input) && typeof input.efx === 'function' ? input.efx :
     typeof efx === 'function' ? efx : () => {};
 
+   /** @type {number|null} */
    let initialValue;
 
-   function onFocusIn(event)
+   /**
+    * Save initial value on focus. Convert to number or null the same way that Svelte does for binding `value`.
+    */
+   function onFocusIn()
    {
-      initialValue = localOptions.cancelOnEscKey ? inputEl.value : void 0;
+      if (localOptions.cancelOnEscKey)
+      {
+         initialValue = inputEl.value === '' ? null : globalThis.parseFloat(inputEl.value);
+      }
    }
 
    /**
@@ -127,12 +127,13 @@
          event.preventDefault();
          event.stopPropagation();
 
-         inputEl.blur(); return;
+         inputEl.blur();
+         return;
       }
 
       if (event.key === 'Escape')
       {
-         if (localOptions.cancelOnEscKey && typeof initialValue === 'string')
+         if (localOptions.cancelOnEscKey && (initialValue === null || typeof initialValue === 'number'))
          {
             event.preventDefault();
             event.stopPropagation();
@@ -141,24 +142,19 @@
             initialValue = void 0;
             inputEl.blur();
          }
-         else if (localOptions.clearOnEscKey)
-         {
-            event.preventDefault();
-            event.stopPropagation();
-
-            store.set('');
-            inputEl.blur();
-         }
       }
    }
 </script>
 
 <div class=tjs-input-container use:efx use:applyStyles={styles}>
     <input class=tjs-input
-           {...{ type }}
+           type=number
            bind:this={inputEl}
            bind:value={$store}
            class:is-value-invalid={!$storeIsValid}
+           max={max}
+           min={min}
+           step={step}
            use:autoBlur
            {placeholder}
            {disabled}
@@ -170,18 +166,18 @@
 <style>
     .tjs-input-container {
         pointer-events: none;
-        background: var(--tjs-input-text-background, var(--tjs-input-background));
-        border-radius: var(--tjs-input-text-border-radius, var(--tjs-input-border-radius));
+        background: var(--tjs-input-number-background, var(--tjs-input-background));
+        border-radius: var(--tjs-input-number-border-radius, var(--tjs-input-border-radius));
         display: block;
         overflow: hidden;
-        margin: var(--tjs-input-text-margin, var(--tjs-input-margin));
-        height: var(--tjs-input-text-height, var(--tjs-input-height));
-        width: var(--tjs-input-text-width, var(--tjs-input-width));
+        margin: var(--tjs-input-number-margin, var(--tjs-input-margin));
+        height: var(--tjs-input-number-height, var(--tjs-input-height));
+        width: var(--tjs-input-number-width, var(--tjs-input-width));
         transform-style: preserve-3d;
     }
 
     .is-value-invalid {
-        color: var(--tjs-input-text-value-invalid-color, var(--tjs-input-value-invalid-color, red));
+        color: var(--tjs-input-number-value-invalid-color, var(--tjs-input-value-invalid-color, red));
     }
 
     input {
@@ -192,26 +188,26 @@
 
         background: transparent;
 
-        border: var(--tjs-input-text-border, var(--tjs-input-border));
-        border-radius: var(--tjs-input-text-border-radius, var(--tjs-input-border-radius));
+        border: var(--tjs-input-number-border, var(--tjs-input-border));
+        border-radius: var(--tjs-input-number-border-radius, var(--tjs-input-border-radius));
 
         width: 100%;
         height: 100%;
 
-        padding: var(--tjs-input-text-padding, var(--tjs-input-padding, initial));
+        padding: var(--tjs-input-number-padding, var(--tjs-input-padding, initial));
 
         color: inherit;
         font-family: inherit;
         font-size: inherit;
         line-height: inherit;
-        text-align: var(--tjs-input-text-text-align, var(--tjs-input-text-align));
+        text-align: var(--tjs-input-number-text-align, var(--tjs-input-text-align));
 
-        cursor: var(--tjs-input-text-cursor, var(--tjs-input-cursor, text));
+        cursor: var(--tjs-input-number-cursor, var(--tjs-input-cursor, text));
 
         transform: translateZ(1px);
     }
 
     input::placeholder {
-        color: var(--tjs-input-text-placeholder-color, var(--tjs-input-placeholder-color, inherit));
+        color: var(--tjs-input-number-placeholder-color, var(--tjs-input-placeholder-color, inherit));
     }
 </style>
