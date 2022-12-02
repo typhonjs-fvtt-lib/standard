87fa4ca53d0a9087bbd55cf68b3357739eb337b6
typhonrt <support@typhonjs.io>
2022-12-02 07:38:12 -0800

    CQ for inner spin button

--- src/component/standard/color/picker-colord/base/text/TextInput.svelte
+++ src/component/standard/color/picker-colord/base/text/TextInput.svelte
@@ -19,6 +19,9 @@
     .text-input {
        display: flex;
        flex-direction: column;
+
+       --tjs-input-text-align: center;
+       --tjs-input-width: 5px;
     }
 
     @container tjs-color-picker-container (min-width: 0) {
@@ -28,15 +31,30 @@
        }
     }
 
-    @container tjs-color-picker-container (min-width: 235px) {
-       .text-input {
-          font-size: 1em;
-       }
-    }
-
     @container tjs-color-picker-container (0 <= width < 235px) {
        .text-input {
           font-size: max(0.5em, 0.5em + 2.98cqi);
+          --tjs-input-height: max(13px, 13px + 5cqi);
+
+          /* For Firefox */
+          --tjs-input-number-appearance: textfield;
+
+          /* For Webkit */
+          --tjs-input-number-webkit-inner-spin-button-appearance: none;
+          --tjs-input-number-webkit-inner-spin-button-opacity: 0;
+       }
+    }
+
+    @container tjs-color-picker-container (min-width: 235px) {
+       .text-input {
+          font-size: 1em;
+          --tjs-input-height: 26px;
+
+          /* For Firefox */
+          --tjs-input-number-appearance: auto;
+
+          /* For Webkit */
+          --tjs-input-number-webkit-inner-spin-button-opacity: 1;
        }
     }
 </style>
