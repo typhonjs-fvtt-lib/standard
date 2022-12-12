8d218c97dd1f278d74dbfbe9af5479b3fd48479e
typhonrt <support@typhonjs.io>
2022-12-12 12:58:53 -0800

    ripple efx takes an events list.

--- src/action/animation/composable/ripple.js
+++ src/action/animation/composable/ripple.js
@@ -1,6 +1,4 @@
-import {
-   debounce as debounceFn,
-   isIterable } from '@typhonjs-svelte/lib/util';
+import { debounce as debounceFn } from '@typhonjs-svelte/lib/util';
 
 /**
  * Defines the classic Material Design ripple effect as an action. `ripple` is a wrapper around the returned action.
