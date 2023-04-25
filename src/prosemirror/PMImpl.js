3a7730c626eb2ef0bbf982fca65478034b8a3204
typhonrt <support@typhonjs.io>
2023-04-25 02:46:57 -0700

    Move clamp to @typhonjs-svelte/lib

--- src/prosemirror/PMImpl.js
+++ src/prosemirror/PMImpl.js
@@ -1,25 +1,10 @@
+import { clamp } from '#runtime/svelte/math';
+
 /**
  * Provides helper utilities to manipulate the PM editor view for various options from TJSProseMirror component.
  */
 export class PMImpl
 {
-   /**
-    * Clamps a value between min / max values.
-    *
-    * @param {number}   value - Value to clamp.
-    *
-    * @param {number}   min - Minimum value.
-    *
-    * @param {number}   max - Maximum value.
-    *
-    * @returns {number} Clamped value.
-    * TODO: Move Math utility function to @typhonjs-svelte/lib
-    */
-   static #clamp(value = 0, min = 0, max = 0)
-   {
-      return Math.min(Math.max(value, min), max);
-   }
-
    /**
     * Handles `options.initialSelection`: Sets the initial cursor / selection range to the start, end, or selects
     * all text.
@@ -47,24 +32,24 @@ export class PMImpl
       {
          case 'all':
          {
-            const resolvedFrom = this.#clamp(0, minPos, maxPos);
-            const resolvedEnd = this.#clamp(doc.content.size, minPos, maxPos);
+            const resolvedFrom = clamp(0, minPos, maxPos);
+            const resolvedEnd = clamp(doc.content.size, minPos, maxPos);
             transaction = tr.setSelection(globalThis.ProseMirror.TextSelection.create(doc, resolvedFrom, resolvedEnd));
             break;
          }
 
          case 'end':
          {
-            const resolvedFrom = this.#clamp(doc.content.size, minPos, maxPos);
-            const resolvedEnd = this.#clamp(doc.content.size, minPos, maxPos);
+            const resolvedFrom = clamp(doc.content.size, minPos, maxPos);
+            const resolvedEnd = clamp(doc.content.size, minPos, maxPos);
             transaction = tr.setSelection(globalThis.ProseMirror.TextSelection.create(doc, resolvedFrom, resolvedEnd));
             break;
          }
 
          case 'start':
          {
-            const resolvedFrom = this.#clamp(0, minPos, maxPos);
-            const resolvedEnd = this.#clamp(0, minPos, maxPos);
+            const resolvedFrom = clamp(0, minPos, maxPos);
+            const resolvedEnd = clamp(0, minPos, maxPos);
             transaction = tr.setSelection(globalThis.ProseMirror.TextSelection.create(doc, resolvedFrom, resolvedEnd));
             break;
          }
