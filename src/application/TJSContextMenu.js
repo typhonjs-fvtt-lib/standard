09af017a0394bcea0cb3f97dbf57ea24f13dee8e
typhonrt <support@typhonjs.io>
2023-11-03 20:15:26 -0700

    Pass event to onPress callbacks.

--- src/application/TJSContextMenu.js
+++ src/application/TJSContextMenu.js
@@ -138,7 +138,7 @@ export class TJSContextMenu
 
       // Register an event listener to remove any active context menu if closed from a menu selection or pointer
       // down event to `document.body`.
-      this.#contextMenu.$on('close', () => { this.#contextMenu = void 0; });
+      this.#contextMenu.$on('close:contextmenu', () => { this.#contextMenu = void 0; });
    }
 
    /**
