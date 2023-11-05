7af21002f482bd11346a8a80be9adc4f9940ae2f
typhonrt <support@typhonjs.io>
2023-11-05 00:55:43 -0700

    Added TJSSlotButton.

--- src/component/standard/button/index.js
+++ src/component/standard/button/index.js
@@ -1,2 +1,3 @@
 export { default as TJSIconButton }       from './TJSIconButton.svelte';
+export { default as TJSSlotButton }       from './TJSSlotButton.svelte';
 export { default as TJSToggleIconButton } from './TJSToggleIconButton.svelte';
