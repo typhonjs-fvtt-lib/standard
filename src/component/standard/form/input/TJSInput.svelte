b360b344c3fcd346900d0a1473c56aa68bfb8909
typhonrt <support@typhonjs.io>
2023-11-05 20:11:48 -0800

    Added label support. props passthrough.

--- src/component/standard/form/input/TJSInput.svelte
+++ src/component/standard/form/input/TJSInput.svelte
@@ -1,10 +1,17 @@
 <script>
    /**
-    * Provides a generic "input" component that creates the specific input component based on 'type'.
+    * Provides a generic "input" component that creates the specific input component based on 'type'. If no `type`
+    * property is available in the input object `text` is the default.
     *
-    * You must provide the configuration solely through the `input` prop which is passed onto the actual implementation.
+    * You may optionally define a label either as `input.label` or through the `label` prop as a string. The label
+    * element uses `display: contents` which ignores the label element and lays out the children as if the
+    * label element does not exist which is perfect for a grid layout.
     *
-    * If no `type` property is available in the input object `text` is assumed as the default.
+    * The following CSS variables control the associated styles with the default values.
+    *
+    * --tjs-input-label-display - content
+    * --tjs-input-label-text-align - right
+    * --tjs-input-label-white-space - nowrap
     */
 
    import { isObject }          from '#runtime/util/object';
@@ -13,6 +20,11 @@
    import TJSInputText          from './TJSInputText.svelte';
 
    export let input = void 0;
+
+   export let label = void 0;
+
+   $: label = isObject(input) && typeof input.label === 'string' ? input.label :
+    typeof label === 'string' ? label : void 0;
 
    let component;
 
@@ -40,4 +52,32 @@
    }
 </script>
 
-<svelte:component this={component} {input} />
+{#if label}
+   <!-- svelte-ignore a11y-label-has-associated-control -->
+   <label class=tjs-input-label>
+      <slot name=label-before />
+
+      {#if typeof label === 'string'}
+         <span class=tjs-input-label-span>{label}</span>
+      {/if}
+
+      <slot name=label-after />
+
+      <svelte:component this={component} {...$$props} />
+
+      <slot />
+   </label>
+{:else}
+   <svelte:component this={component} {...$$props} />
+{/if}
+
+<style>
+   label {
+      display: var(--tjs-input-label-display, contents);
+   }
+
+   span {
+      text-align: var(--tjs-input-label-text-align, right);
+      white-space: var(--tjs-input-label-white-space, nowrap);
+   }
+</style>
