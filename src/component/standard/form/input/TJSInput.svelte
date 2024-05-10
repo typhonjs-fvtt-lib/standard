c5b9c2608924069e0e9cd005284b0d62e72f4ba0
typhonrt <support@typhonjs.io>
2024-05-10 13:45:33 -0700

    Fix prop forwarding; remove Vite dev server warnings.

--- src/component/standard/form/input/TJSInput.svelte
+++ src/component/standard/form/input/TJSInput.svelte
@@ -34,6 +34,12 @@
 
    let component;
 
+   // Some components need to reassign `input` to another prop name. Warnings are only generated w/ the Vite dev server.
+   let passedProps = Object.assign({}, $$props);
+
+   // Remove the `type` prop used locally.
+   delete passedProps.type;
+
    $: {
       type = isObject(input) && typeof input.type === 'string' ? input.type :
        typeof type === 'string' ? type : 'text';
@@ -41,6 +47,10 @@
       switch (type)
       {
          case 'button':
+            // Reassign prop `input` to `button`.
+            passedProps.button = input;
+            delete passedProps.input;
+
             component = TJSButton;
             break;
 
@@ -66,6 +76,10 @@
 
 
          case 'select':
+            // Reassign prop `input` to `select`.
+            passedProps.select = input;
+            delete passedProps.input;
+
             component = TJSSelect;
             break;
 
@@ -77,11 +91,11 @@
 </script>
 
 {#if type === 'button'}
-   <svelte:component this={component} on:click on:press on:contextmenu {...$$props} button={input} />
+   <svelte:component this={component} on:click on:press on:contextmenu {...passedProps} />
 {:else if type === 'select'}
-   <svelte:component this={component} {...$$props} select={input} />
+   <svelte:component this={component} {...passedProps} />
 {:else}
-   <svelte:component this={component} {...$$props} />
+   <svelte:component this={component} {...passedProps} />
 {/if}
 
 
