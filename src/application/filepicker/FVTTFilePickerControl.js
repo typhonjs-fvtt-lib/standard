4260bc1af0bdbf49eea7278efc42e6a34897c1ac
typhonrt <support@typhonjs.io>
2024-09-30 18:05:14 -0700

    Refactor add #runtime/util/a11y sub-path.

--- src/application/filepicker/FVTTFilePickerControl.js
+++ src/application/filepicker/FVTTFilePickerControl.js
@@ -2,9 +2,9 @@ import { fade }               from 'svelte/transition';
 
 import { TJSDialog }          from '#runtime/svelte/application';
 import { TJSSvelteUtil }      from '#runtime/svelte/util';
-import { ManagedPromise }     from '#runtime/util/async';
 import { nextAnimationFrame } from '#runtime/util/animate';
-import { A11yHelper }         from '#runtime/util/browser';
+import { A11yHelper }         from '#runtime/util/a11y';
+import { ManagedPromise }     from '#runtime/util/async';
 import {
    isIterable,
    isObject }                 from '#runtime/util/object';
