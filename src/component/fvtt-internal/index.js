5d6e63379f452439b1ad2b8bb1849de91bbbb044
typhonrt <support@typhonjs.io>
2025-01-04 02:58:10 -0800

    Hide fvtt internal components from API docs.

--- src/component/fvtt-internal/index.js
+++ src/component/fvtt-internal/index.js
@@ -1,2 +1,10 @@
+/**
+ * This package contains internal component resources for TRL. They are exposed through other API aspects and should
+ * not be directly accessed.
+ *
+ * @packageDocumentation
+ *
+ * @hidden
+ */
 export * from './dialog';
 export * from './sidebar';
