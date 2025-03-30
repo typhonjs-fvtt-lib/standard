03f6790b3300e33ab6254cf40729108d4cb74293
typhonrt <support@typhonjs.io>
2025-03-29 19:59:03 -0700

    Temporarily exclude MCE component from export. Pending v13 CSS layers styles fix for MCE.

--- src/component/fvtt/editor/index.js
+++ src/component/fvtt/editor/index.js
@@ -2,4 +2,8 @@ import '#internal/configure';
 
 export * from './content-editable/index.js';
 export * from './prosemirror/index.js';
-export * from './tinymce/index.js';
+
+// TODO: TinyMCE support is currently disabled for v13 until default MCE CSS layer style fix is available.
+// See: https://github.com/foundryvtt/foundryvtt/issues/12352
+// export * from './tinymce/index.js';
+
