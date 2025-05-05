e840cf03e5650dba4bc42033cab71f16bf5f6154
typhonrt <support@typhonjs.io>
2025-05-05 09:19:38 -0700

    Remove MCE support.

--- src/component/fvtt/editor/index.js
+++ src/component/fvtt/editor/index.js
@@ -3,7 +3,3 @@ import '#internal/configure';
 export * from './content-editable/index.js';
 export * from './prosemirror/index.js';
 
-// TODO: TinyMCE support is currently disabled for v13 until default MCE CSS layer style fix is available.
-// See: https://github.com/foundryvtt/foundryvtt/issues/12352
-// export * from './tinymce/index.js';
-
