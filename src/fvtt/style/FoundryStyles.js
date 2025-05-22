329364b8fa61156ba477646b1d0576c06a647801
typhonrt <support@typhonjs.io>
2025-05-22 15:14:10 -0700

    FoundryStyles / part 2

--- src/fvtt/style/FoundryStyles.js
+++ src/fvtt/style/FoundryStyles.js
@@ -1,10 +1,14 @@
 import { isObject } from '#runtime/util/object';
 
 /**
- * Parses the core Foundry style sheet creating an indexed object of properties by selector parts.
+ * Parses the core Foundry style sheet creating an indexed object of properties by individual selector parts that are
+ * viable to use for specific element styling.
  */
 export class FoundryStyles
 {
+   /**
+    * @type {Map<string, []>} Allowed fully qualified CSS layers to parse.
+    */
    static #ALLOWED_LAYERS = new Map([
       ['variables.base', []],
       ['variables.themes.general', []],
@@ -12,12 +16,16 @@ export class FoundryStyles
       ['elements.forms', []]
    ]);
 
+   /**
+    * @type {RegExp[]} Array of regexes to reduce selector parts tracked.
+    */
    static #DISALLOWED_PARTS_ANY = [
       />\s*[^ ]+/,            // Direct child selectors
       /(^|\s)\*/,             // Universal selectors
       /(^|\s)\.app(?![\w-])/, // AppV1 class
       /^\.application\.theme/,
       /^body\.auth/,
+      /^body(?:\.[\w-]+)*\.application\b/,  // Remove unnecessary `body.<theme>.application` pairing.
       /code-?mirror/i,
       /#camera-views/,
       /\.chat-message/,
@@ -34,10 +42,11 @@ export class FoundryStyles
       /\.window-app/,
    ];
 
-   static #logEnabledEntries = false;
-   static #logEnabledKeys = false;
-   static #logEnabledParts = false;
-
+   /**
+    * Foundry stylesheet.
+    *
+    * @type {CSSStyleSheet}
+    */
    static #sheet = void 0;
 
    /** @type {Map<string, {[key: string]: string}>} */
@@ -45,8 +54,119 @@ export class FoundryStyles
 
    static #initialized = false;
 
-   // TODO REMOVE
-   static init() { this.#initialize(); }
+   /**
+    * @returns {MapIterator<[string, {[p: string]: string}]>} Tracked CSS selector key / value iterator.
+    */
+   static entries()
+   {
+      if (!this.#initialized) { this.#initialize(); }
+
+      return this.#sheetMap.entries();
+   }
+
+   /**
+    * Gets all properties associated with the selector. Try and use a direct match otherwise all keys
+    * are iterated to find a selector string that includes the `selector`.
+    *
+    * @param {string}   selector - Selector to find.
+    *
+    * @returns { {[key: string]: string} } Properties object.
+    */
+   static get(selector)
+   {
+      if (!this.#initialized) { this.#initialize(); }
+
+      // If there is a direct selector match then return a value immediately.
+      if (this.#sheetMap.has(selector))
+      {
+         return this.#sheetMap.get(selector);
+      }
+
+      for (const key of this.#sheetMap.keys())
+      {
+         if (key.includes(selector)) { return this.#sheetMap.get(key); }
+      }
+
+      return void 0;
+   }
+
+   /**
+    * Gets a specific property value from the given `selector` and `property` key. Try and use a direct selector
+    * match otherwise all keys are iterated to find a selector string that includes `selector`.
+    *
+    * @param {string}   selector - Selector to find.
+    *
+    * @param {string}   property - Specific property to locate.
+    *
+    * @returns {string | undefined} Property value.
+    */
+   static getProperty(selector, property)
+   {
+      if (!this.#initialized) { this.#initialize(); }
+
+      // If there is a direct selector match then return a value immediately.
+      if (this.#sheetMap.has(selector))
+      {
+         const data = this.#sheetMap.get(selector);
+
+         return isObject(data) && property in data ? data[property] : void 0;
+      }
+
+      for (const key of this.#sheetMap.keys())
+      {
+         if (key.includes(selector))
+         {
+            const data = this.#sheetMap.get(key);
+            if (isObject(data) && property in data) { return data[property]; }
+         }
+      }
+
+      return void 0;
+   }
+
+   /**
+    * @param {string}   selector - CSS selector to check.
+    *
+    * @returns {boolean} FoundryStyles tracks the given selector.
+    */
+   static has(selector)
+   {
+      if (!this.#initialized) { this.#initialize(); }
+
+      return this.#sheetMap.has(selector);
+   }
+
+   /**
+    * @returns {MapIterator<string>} Tracked CSS selector keys iterator.
+    */
+   static keys()
+   {
+      if (!this.#initialized) { this.#initialize(); }
+
+      return this.#sheetMap.keys();
+   }
+
+   /**
+    * @returns {CSSStyleSheet} Main Foundry stylesheet.
+    */
+   static get sheet()
+   {
+      if (!this.#initialized) { this.#initialize(); }
+
+      return this.#sheet;
+   }
+
+   /**
+    * @returns {number} Returns the size / count of selector properties tracked.
+    */
+   static get size()
+   {
+      if (!this.#initialized) { this.#initialize(); }
+
+      return this.#sheetMap.size;
+   }
+
+   // Internal Implementation ----------------------------------------------------------------------------------------
 
    /**
     * Called once on initialization / first usage. Parses the core foundry style sheet.
@@ -56,8 +176,6 @@ export class FoundryStyles
       this.#initialized = true;
 
       const styleSheets = Array.from(document.styleSheets).filter((entry) => entry.href !== null);
-
-      let sheet;
 
       const foundryStyleSheet = globalThis.foundry.utils.getRoute('/css/foundry2.css');
 
@@ -74,34 +192,22 @@ export class FoundryStyles
 
          if (typeof url.pathname === 'string' && url.pathname === foundryStyleSheet)
          {
-            this.#sheet = sheet = styleSheet;
+            this.#sheet = styleSheet;
             break;
          }
       }
 
       // Quit now if the Foundry style sheet was not found.
-      if (!sheet) { return; }
+      if (!this.#sheet) { return; }
 
       // Parse each CSSStyleRule and build the map of selectors to parsed properties.
-      for (const layerRule of sheet.cssRules)
+      for (const layerRule of this.#sheet.cssRules)
       {
          if (!(layerRule instanceof CSSLayerBlockRule)) { continue; }
          if (!isObject(layerRule.cssRules)) { continue; }
          if (layerRule?.name === 'reset') { continue; }
 
          this.#processLayerBlockRule(layerRule);
-      }
-
-      if (this.#logEnabledKeys)
-      {
-         console.log(`!!! FoundryStyles - #initialize - 1 - this.#sheetMap keys:`);
-         console.log(JSON.stringify(Array.from(this.#sheetMap.keys()), null, 2));
-      }
-
-      if (this.#logEnabledEntries)
-      {
-         console.log(`!!! FoundryStyles - #initialize - 1 - this.#sheetMap entries:`);
-         console.log(JSON.stringify(Object.fromEntries(this.#sheetMap), null, 2));
       }
    }
 
@@ -136,11 +242,6 @@ export class FoundryStyles
 
       const layerBlockRules = [];
 
-      if (this.#logEnabledParts && this.#ALLOWED_LAYERS.has(fullname))
-      {
-         console.log(`\n\n!!! FoundryStyles - #processLayerBlockRule - fullname: `, fullname);
-      }
-
       for (const rule of blockRule.cssRules)
       {
          if (rule instanceof CSSLayerBlockRule) { layerBlockRules.push(rule); }
@@ -160,99 +261,24 @@ export class FoundryStyles
     */
    static #processStyleRule(styleRule)
    {
-      if (this.#logEnabledParts) { console.log(styleRule.selectorText); }
+      if (typeof styleRule.selectorText !== 'string') { return; }
 
+      const result = this.#parseCssText(styleRule.cssText);
 
-      if (typeof styleRule.selectorText === 'string')
+      // Split selector parts and remove disallowed selector parts and empty strings.
+      const selectorParts = styleRule.selectorText.split(',')
+         .map((str) => str.trim())
+         .filter((str) => !this.#DISALLOWED_PARTS_ANY.some((regex) => regex.test(str)))
+         .filter(Boolean); // Remove empty parts.
+
+      if (selectorParts.length)
       {
-         const result = this.#parseCssText(styleRule.cssText);
-
-         // Split selector parts and remove empty strings.
-         const selectorParts = styleRule.selectorText.split(',')
-            .map((str) => str.trim())
-            .filter((str) => !this.#DISALLOWED_PARTS_ANY.some((regex) => regex.test(str)))
-            .filter(Boolean); // Remove empty parts.
-
-         if (this.#logEnabledParts)
+         for (const part of selectorParts)
          {
-            for (const part of selectorParts) { console.log(`! P: ${part}`); }
-         }
-
-         if (selectorParts.length)
-         {
-            for (const part of selectorParts)
-            {
-               const existing = this.#sheetMap.get(part);
-               const update = Object.assign(existing ?? {}, result);
-               this.#sheetMap.set(part, update);
-            }
+            const existing = this.#sheetMap.get(part);
+            const update = Object.assign(existing ?? {}, result);
+            this.#sheetMap.set(part, update);
          }
       }
-   }
-
-   /**
-    * Gets the properties object associated with the selector. Try and use a direct match otherwise all keys
-    * are iterated to find a selector string that includes the `selector`.
-    *
-    * @param {string}   selector - Selector to find.
-    *
-    * @returns { {[key: string]: string} } Properties object.
-    */
-   static getProperties(selector)
-   {
-      if (!this.#initialized) { this.#initialize(); }
-
-      // If there is a direct selector match then return a value immediately.
-      if (this.#sheetMap.has(selector))
-      {
-         return this.#sheetMap.get(selector);
-      }
-
-      for (const key of this.#sheetMap.keys())
-      {
-         if (key.includes(selector)) { return this.#sheetMap.get(key); }
-      }
-
-      return void 0;
-   }
-
-   /**
-    * Gets a specific property value from the given `selector` and `property` key. Try and use a direct selector
-    * match otherwise all keys are iterated to find a selector string that includes `selector`.
-    *
-    * @param {string}   selector - Selector to find.
-    *
-    * @param {string}   property - Specific property to locate.
-    *
-    * @returns {string|undefined} Property value.
-    */
-   static getProperty(selector, property)
-   {
-      if (!this.#initialized) { this.#initialize(); }
-
-console.log(`!!! FoundryStyles - getProperty - 0 - selector: ${selector}; property: ${property}`)
-
-      // If there is a direct selector match then return a value immediately.
-      if (this.#sheetMap.has(selector))
-      {
-         const data = this.#sheetMap.get(selector);
-
-console.log(`!!! FoundryStyles - getProperty - 1 - isObject(data): ${isObject(data)}; isObject(data) && property in data: ${isObject(data) && property in data}`);
-
-         return isObject(data) && property in data ? data[property] : void 0;
-      }
-
-console.log(`!!! FoundryStyles - getProperty - 2`)
-
-      for (const key of this.#sheetMap.keys())
-      {
-         if (key.includes(selector))
-         {
-            const data = this.#sheetMap.get(key);
-            if (isObject(data) && property in data) { return data[property]; }
-         }
-      }
-
-      return void 0;
    }
 }
