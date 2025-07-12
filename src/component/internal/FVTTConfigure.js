3703e38584b1113ada1cab4c548457801c17bd4d
typhonrt <support@typhonjs.io>
2025-07-12 14:37:54 -0700

    Initial v13 CSS / dynamic style work.

--- src/component/internal/FVTTConfigure.js
+++ src/component/internal/FVTTConfigure.js
@@ -1,9 +1,6 @@
-import {
-   FoundryStyles,
-   SvelteApp }                from '#runtime/svelte/application';
-
-import { TJSStyleManager }    from '#runtime/util/dom/style';
-import { isObject }           from '#runtime/util/object';
+import { FoundryStyles }   from '#runtime/svelte/application';
+import { TJSStyleManager } from '#runtime/util/dom/style';
+import { isObject }        from '#runtime/util/object';
 
 /**
  * Provides global CSS variable configuration based on Foundry styles loaded.
@@ -19,19 +16,23 @@ class FVTTConfigure
       // Remove `0.2.x` and below root styles. -- REMOVE AT `0.5.0`
       document?.['#__tjs-root-styles']?.remove?.();
 
-      const manager = new TJSStyleManager({ id: '__tjs-standard-vars', version: 1, layerName: 'variables.tjs-vars', rules:
-         {
+      const manager = new TJSStyleManager({
+         id: '__tjs-standard-vars',
+         version: 1,
+         layerName: 'variables.tjs-standard-vars',
+         rules: {
             themeDark: ':root, .themed.theme-dark',
             themeLight: '.themed.theme-light'
          }
       });
 
       // Early out if the style manager version is outdated.
-      if (!manager.isConnected()) { return; }
+      if (!manager.isConnected) { return; }
 
-      const cssVariables = manager.get('themeDark');
+      const themeDarkRoot = manager.get('themeDark');
       const themeLight = manager.get('themeLight');
 
+      // TODO THIS IS A TEST / REMOVE
       themeLight.setProperties({
          '--tjs-input-background': 'red',
       }, false);
@@ -41,10 +42,35 @@ class FVTTConfigure
       // -------------------------------------------------------------------------------------------------------------
 
       /**
+       * Assign all TyphonJS thematic CSS reversals for core Foundry styles.
+       */
+      themeDarkRoot.setProperties({
+         // For checkbox Foundry core styles override.
+         '--tjs-input-checkbox-appearance': 'none',
+      }, false);
+
+      // TODO: Consider input component focus / focus-visible support
+      // themeDarkRoot.setProperties({
+      //    // For checkbox Foundry core styles override.
+      //    '--tjs-input-outline': '1px solid transparent',
+      //    // '--tjs-input-outline-focus': '2px solid var(--input-focus-outline-color)',
+      //    '--tjs-input-outline-focus': '2px solid red',
+      //    '--tjs-input-outline-offset': '-1px',
+      //    '--tjs-input-outline-offset-focus': '-2px',
+      //    '--tjs-input-transition': 'outline-color 0.5s',
+      //    // '--tjs-input-transition-focus': 'outline-color 0.5s',
+      //    // '--tjs-input-transition-focus-visible': 'outline-color 0.5s',
+      // }, false);
+
+      /*
+      outline: 2px solid var(--input-focus-outline-color);
+    outline-offset: -2px;
+       */
+      /**
        * Assign all TyphonJS thematic CSS variables.
        */
 
-      cssVariables.setProperties({
+      themeDarkRoot.setProperties({
          // For components w/ transparent background checkered pattern.
          '--tjs-checkerboard-background-dark': 'rgb(205, 205, 205)',
          '--tjs-checkerboard-background-10': `url('data:image/svg+xml;utf8,<svg preserveAspectRatio="none"  viewBox="0 0 10 10" xmlns="http://www.w3.org/2000/svg"><rect x="0" y="0" width="5" height="5" fill="transparent" /><rect x="5" y="5" width="5" height="5" fill="transparent" /><rect x="5" y="0" width="5" height="5" fill="white" /><rect x="0" y="5" width="5" height="5" fill="white" /></svg>') 0 0 / 10px 10px, var(--tjs-checkerboard-background-dark, rgb(205, 205, 205))`
@@ -56,16 +82,21 @@ class FVTTConfigure
        * Assign all TyphonJS CSS variables to Foundry defaults.
        */
 
-      cssVariables.setProperties({
-         '--tjs-action-ripple-background': 'rgba(0, 0, 0, 0.35)'
+      themeDarkRoot.setProperties({
+         '--tjs-action-ripple-background': 'linear-gradient(64.5deg, rgba(245, 116, 185, 1) 40%, rgba(89, 97, 223, 1) 60%)',
+
+         '--tjs-icon-button-background-hover': 'rgba(255, 255, 255, 0.05)',
+         '--tjs-icon-button-background-selected': 'rgba(255, 255, 255, 0.1)',
       }, false);
 
-      // -------------------------------------------------------------------------------------------------------------
+      themeLight.setProperties({
+         '--tjs-action-ripple-background': 'rgba(0, 0, 0, 0.35)',
 
-      cssVariables.setProperties({
          '--tjs-icon-button-background-hover': 'rgba(0, 0, 0, 0.10)',
          '--tjs-icon-button-background-selected': 'rgba(0, 0, 0, 0.20)',
       }, false);
+
+      // -------------------------------------------------------------------------------------------------------------
 
       {
          /**
@@ -73,21 +104,40 @@ class FVTTConfigure
           */
          const propsBody = FoundryStyles.get('body');
          const props = FoundryStyles.get('input[type="text"]');
+         const propsFocus = FoundryStyles.get('input[type="text"]:focus');
 
-         cssVariables.setProperties({
+         console.log(`!!! FVTTConfigure - initialize - props: \n${JSON.stringify(props, null, 2)}`);
+         console.log(`!!! FVTTConfigure - initialize - propsFocus: \n${JSON.stringify(propsFocus, null, 2)}`);
+
+         themeDarkRoot.setProperties({
+            // TODO: VERIFY
             '--tjs-input-background': 'background' in props ? propsBody['--color-cool-4'] : 'var(--color-cool-4)',
-            '--tjs-input-border': 'border' in props ? props.border : '1px solid var(--input-border-color)',
             '--tjs-input-border-radius': 'border-radius' in props ? props['border-radius'] : '4px',
             '--tjs-input-height': '--input-height' in propsBody ? propsBody['--input-height'] : '2rem',
-//               '--tjs-input-min-width': 'min-width' in props ? props['min-width'] : '20px',
             '--tjs-input-padding': 'padding' in props ? props['padding'] : '0px 0.5rem',
             '--tjs-input-width': 'width' in props ? props.width : '100%',
+
+            // TODO: Hook up to Foundry Styles.
+            '--tjs-input-outline': 'outline' in props ? props['outline'] : 'red solid 1px',
+            '--tjs-input-transition': 'transition' in props ? props['transition'] : 'outline-color 2.5s', // 'outline-color 0.5s'
+
+            // TODO: Must substitute core color var.
+            '--tjs-input-outline-focus': 'outline' in propsFocus ? propsFocus['outline'] : 'red solid 2px',
+
+            '--tjs-input-outline-offset-focus': 'outline-offset' in propsFocus ? propsFocus['outline-offset'] : '-5px', // '-2px'
+
+            // '--tjs-input-outline-offset': '-1px',
+            // '--tjs-input-outline-focus-visible': 'none',
+
+            // TODO: VERIFY or REMOVE
+            // '--tjs-input-border': 'border' in props ? props.border : '1px solid var(--input-border-color)',
+            // '--tjs-input-border-color': 'var(--input-border-color)',
+            //   '--tjs-input-min-width': 'min-width' in props ? props['min-width'] : '20px',
 
             // Set default values that are only to be referenced and not set.
             '--_tjs-default-input-height': '--input-height' in propsBody ? propsBody['--input-height'] : 'var(--input-height)',
 
             // Set directly / no lookup:
-            '--tjs-input-border-color': 'var(--input-border-color)',
          }, false);
       }
 
@@ -103,34 +153,34 @@ class FVTTConfigure
 
          if (isObject(propsTrack))
          {
-            cssVariables.setProperties({
+            themeDarkRoot.setProperties({
                '--tjs-input-range-slider-track-box-shadow': 'box-shadow' in propsTrack ? propsTrack['box-shadow'] : '1px 1px 1px #000000, 0px 0px 1px #0d0d0d'
             }, false);
          }
 
          if (isObject(propsTrackFocus))
          {
-            cssVariables.setProperties({
+            themeDarkRoot.setProperties({
                '--tjs-input-range-slider-track-box-shadow-focus': 'box-shadow' in propsTrackFocus ? propsTrackFocus['box-shadow'] : '1px 1px 1px #000000, 0px 0px 1px #0d0d0d'
             }, false);
          }
 
          if (isObject(propsThumb))
          {
-            cssVariables.setProperties({
+            themeDarkRoot.setProperties({
                '--tjs-input-range-slider-thumb-box-shadow': 'box-shadow' in propsThumb ? propsThumb['box-shadow'] : '0 0 5px var(--color-shadow-primary)'
             }, false);
          }
 
          if (isObject(propsThumbFocus))
          {
-            cssVariables.setProperties({
+            themeDarkRoot.setProperties({
                '--tjs-input-range-slider-thumb-box-shadow-focus': 'box-shadow' in propsThumbFocus ? propsThumbFocus['box-shadow'] : '0 0 5px var(--color-shadow-primary)'
             }, false);
          }
       }
 
-      cssVariables.setProperties({
+      themeDarkRoot.setProperties({
          // `popup` is for components that are slightly elevated, but connected to an application;
          // see: TJSMenu / TJSContextMenu / TJSColordPicker
          '--tjs-default-popup-background': 'var(--color-text-dark-header, #23221d)',
@@ -144,15 +194,13 @@ class FVTTConfigure
          '--tjs-default-popover-box-shadow': '0 0 10px var(--color-shadow-dark, #000)',
       }, false);
 
-      // Handle `PopOut!` module hooks to allow applications to popout to their own browser window -------------------
+      // Handle `PopOut!` module hooks to allow applications to pop out to their own browser window ------------------
 
       Hooks.on('PopOut:loading', (app, popout) =>
       {
-         if (app instanceof SvelteApp)
-         {
-            // Clone and load `svelte-standard` CSS variables into new window document.
-            popout.document.addEventListener('DOMContentLoaded', () => cssVariables.clone(popout.document));
-         }
+         // Clone and load the `standard` library CSS variables into the new window document regardless of the app type.
+         popout.document.addEventListener('DOMContentLoaded',
+         () => manager.clone({ document: popout.document, force: true }));
       });
    }
 }
