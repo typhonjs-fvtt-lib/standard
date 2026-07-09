72bc03e59cfacb8ec0b39deb6bdfe5154277ca08
typhonrt <support@typhonjs.io>
2026-07-08 17:43:23 -0700

    Removed SettingEntryDataField for TJSDataFieldGroup.

--- src/component/fvtt/settings/TJSSettingsEdit.svelte
+++ src/component/fvtt/settings/TJSSettingsEdit.svelte
@@ -93,7 +93,7 @@
          <section class="tjs-settings-edit-section tjs-settings-edit-content">
             {#each uiSettings.topLevel as setting (setting.key)}
                {#if setting.datafield}
-                  <TJSDataFieldGroup datafield={setting.datafield} groupConfig={{ rootId: setting.id, label: setting.name, hint: setting.hint, units: setting.units }} store={setting.store} />
+                  <TJSDataFieldGroup datafield={setting.datafield} groupConfig={{ label: setting.name, hint: setting.hint, units: setting.units }} store={setting.store} />
                {:else}
                   <SettingEntry {setting} />
                {/if}
@@ -106,7 +106,7 @@
                <section class=tjs-settings-edit-content>
                   {#each folder.settings as setting (setting.key)}
                      {#if setting.datafield}
-                        <TJSDataFieldGroup datafield={setting.datafield} groupConfig={{ rootId: setting.id, label: setting.name, hint: setting.hint, units: setting.units }} store={setting.store} />
+                        <TJSDataFieldGroup datafield={setting.datafield} groupConfig={{ label: setting.name, hint: setting.hint, units: setting.units }} store={setting.store} />
                      {:else}
                         <SettingEntry {setting} />
                      {/if}
