22222dca7afc6ed4a25b9f7b861cf71d4730bd83
typhonrt <support@typhonjs.io>
2026-07-11 13:24:02 -0700

    Remove unused import.

--- src/store/fvtt/settings/UIControlImpl.ts
+++ src/store/fvtt/settings/UIControlImpl.ts
@@ -4,7 +4,6 @@ import { TJSDialog }             from '#runtime/svelte/application';
 import { TJSSvelte }             from '#runtime/svelte/util';
 import { localize }              from '#runtime/util/i18n';
 import { isObject }              from '#runtime/util/object';
-import { CrossRealm }            from '#runtime/util/realm';
 
 import {
    ripple,
